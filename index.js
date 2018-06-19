fbd69bb576083183a2706123b8400dbfaff7c69b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-19 14:28:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function getFirstSelector(selector) {
+   return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  //Define a function nestedTarget() that pulls a .target out of #nested
+   var target = document.querySelectorAll('#nested .target');
+   
+   
+   return target
+}
