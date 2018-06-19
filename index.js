7d4f35624e63f90495106e1cc4f36dc07b3e47f6
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-19 14:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,5 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   //Define a function nestedTarget() that pulls a .target out of #nested
    var target = document.querySelectorAll('#nested .target');
-  for (let i = 0; i < target.length; i++) {
-  target[i] = (i + 1).toString()
-}
-  
+
 }
