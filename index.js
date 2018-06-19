027385af3401ee5545e6c8a3d183c241758a656c
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-19 14:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   //Define a function nestedTarget() that pulls a .target out of #nested
    var target = document.querySelectorAll('#nested .target');
   for (let i = 0; i < target.length; i++) {
-  lis[i].innerHTML = (i + 1).toString()
+  target[i].innerHTML = (i + 1).toString()
 }
   
 }
